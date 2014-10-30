98cb8aefd04ada0514d87ba7c4b19e84ff788c35
Jeremy Jahn <jeremy.jahn@lookout.com>
2014-10-30 14:41:15 -0700

    exercise 4/7 lesson 1
    A lesson on how to repeat for more input from the user

---
+++ repeat_for_more_input.rb
@@ -0,0 +1,11 @@
+print "What's your first name?"
+first_name = gets.chomp
+
+print "What's your last name?"
+last_name = gets.chomp
+
+print "What city do you live in?"
+city = gets.chomp
+
+print "What state do you live in (e.g. NY, CA, etc)?"
+state = gets.chomp
